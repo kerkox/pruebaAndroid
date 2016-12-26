480dd0fc741c708236ac40c4470c5192af971b26
paul <paulkerg@gmail.com>
2016-12-26 18:15:40 -0500

    Version 0.0.3 +Pruebas de Funcionamiento con Spinner

--- app/src/main/java/com/example/root/holamundo/MainActivity.java
+++ app/src/main/java/com/example/root/holamundo/MainActivity.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button boton;
     EditText editText;
     RadioGroup radioGroup;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +30,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boton = (Button) findViewById(R.id.boton1);
         boton.setOnClickListener(this);
         editText = (EditText) findViewById(R.id.texto);
+        //Radiop Button
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener(new ListenerRadioButton());
+        //Spiner
+        spinner = (Spinner) findViewById(R.id.spinner);
+        List list = new ArrayList();
+        list.add("item 1");
+        list.add("item 2");
+        list.add("item 3");
+        list.add("item 4");
+        list.add("item 5");
+        list.add("item 6");
+        list.add("item 7");
+        list.add("item 8");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,list);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"Position "+String.valueOf(spinner.getSelectedItem()),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
 
