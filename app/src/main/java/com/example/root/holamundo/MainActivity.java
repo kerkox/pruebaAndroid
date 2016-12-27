1aa7b7feb3b898417845603ebe52c7d78878d54b
paul <paulkerg@gmail.com>
2016-12-26 19:13:25 -0500

    Version 0.0.7 +Pruebas de Funcionamiento con Cuadro de Dialogo

--- app/src/main/java/com/example/root/holamundo/MainActivity.java
+++ app/src/main/java/com/example/root/holamundo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.root.holamundo;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,13 +24,38 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button boton;
+    Context context =  this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //**************************************
+        boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setMessage("Mensaje")
+                        .setCancelable(false)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //lo que quieras hacer cuando se da clic a si
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //lo que quieras hacer cuando se da clic a no
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
 
+            }
+        });
 
     }
 
